3fcbd6eb77844f37af7d2d382323538d73208a51
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2021-12-25 21:36:37 +0300

    +: featured

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -2,20 +2,32 @@ import React from 'react';
 import './featured.css';
 import Slider from '../Slider/Slider';
 
-
 const Featured = () => {
   return (
     <section className="featured">
-      <h1>Рекомендуемое</h1>
       <div className="featured__container">
         <div className="featured__left">
-
+        <h1>Рекомендуемое</h1>
           <Slider />
-
 
           <div className="featured__text-container">
             <h2>Название</h2>
-            <p>Три работы Энди Ворхола</p>
+            <p>
+              A new portfolio of five original prints by Willard Boepple,
+              Stephen Chambers, Nigel Hall, Peter Randall-Page and Alison
+              Wilding.
+            </p>
+            <p>
+              The ‘folio is being released for sale at an initial price of
+              £1,200 making it an unprecedented opportunity to acquire a
+              selection of fine prints by leading contemporary artists.
+            </p>
+            <button type="button" className="fullwidth">
+              <a href="./">Click here for more information</a>
+            </button>
+            <button type="button">
+              <a href="./">ЗАПРОС</a>
+            </button>
           </div>
         </div>
 
