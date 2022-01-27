324b86533f9bb39f2e011883de21a26cc6884379
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2022-01-27 19:47:08 +0300

    -: further

--- src/components/MainPageContent/MainPageContent.js
+++ src/components/MainPageContent/MainPageContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Display from '../Display/Display';
 import Featured from '../Featured/Featured';
-import Further from '../Further/Further';
+// import Further from '../Further/Further';
 import Video from '../Video/Video';
 
 const MainPageContent = () => {
